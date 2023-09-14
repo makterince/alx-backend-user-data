3fbbbda3a9580dfcaf9c6960229edff9380874fe
Festus Omaku <fomaku3@gmail.com>
2023-09-14 17:27:01 +0100

    user table

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -30,3 +30,9 @@ class User(Base):
     hashed_password = Column(String, nullable=False)
     session_id = Column(String, nullable=True)
     reset_token = Column(String, nullable=True)
+
+    def __repr__(self):
+        return "<User(id='%d', email='%s', hashed_password='%s'
+                session_id='%s', reset_token='%s')>" % (
+                    self.id, self.email, self.hashed_password,
+                    self.session_id, self.reset_token)
